915a2a9ff13a6ef65154c89a1b17d81112daeab4
Andrey Lushnikov <lushnikov@chromium.org>
2020-06-26 09:53:10 -0700

    demo request interception

---
+++ requestinterception.js
@@ -0,0 +1,20 @@
+const {chromium, webkit, firefox, devices} = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  page.on('frameattached', frame => console.log('frames: ' + page.frames().length));
+  page.on('framedetached', frame => console.log('frames: ' + page.frames().length));
+  page.on('request', request => console.log(`>>> ${request.method()} ${request.url()}`));
+  page.on('response', response => console.log(`<<< ${response.status()} ${response.url()}`));
+  context.route('**/*', route => {
+    if (route.request().frame().parentFrame())
+      route.abort();
+    else
+      route.continue();
+  });
+  await page.goto('https://theverge.com');
+})();
