1c028a14da8cd690312ac8cc25a2c5779f7f5668
Andrey Lushnikov <lushnikov@chromium.org>
2020-06-26 09:47:39 -0700

    locale & colorscheme

---
+++ locale.js
@@ -0,0 +1,23 @@
+const {chromium, webkit, firefox, devices} = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    proxy: {
+      server: 'http://proxy.aslushnikov.com:3128',
+      username: process.env.PROXY_USERNAME,
+      password: process.env.PROXY_PASSWORD,
+    },
+  });
+  const context = await browser.newContext({
+    geolocation: {
+      latitude: 51.5080647,
+      longitude: -0.0993812,
+    },
+    permissions: ['geolocation'],
+    locale: 'de-DE',
+    colorScheme: 'light',
+  });
+  const page = await context.newPage();
+  await page.goto('https://overreacted.io');
+})();
